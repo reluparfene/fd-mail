9f7c8290fe69931545545ff70ce326f39166f9e9
Relu Parfene <relu.parfene@noit.ro>
2020-08-16 00:22:11 +0300

    Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   fd-mail-init/Dockerfile
    	modified:   fd-mail-init/docker-entrypoint.sh
    	new file:   fd-mail-init/fd.conf
    	new file:   fd-mail-init/fusiondirectory.conf
    	modified:   fd.yml

--- fd-mail-init/docker-entrypoint.sh
+++ fd-mail-init/docker-entrypoint.sh
@@ -3,5 +3,6 @@ sed -i -e "s/LDAP_BASE/${LDAP_BASE}/g" /etc/fusiondirectory/fusiondirectory.conf
 sed -i -e "s/LDAP_HOST/${LDAP_HOST}/g" /etc/fusiondirectory/fusiondirectory.conf
 sed -i -e "s/LDAP_ADMIN_PASS/${LDAP_ADMIN_PASS}/g" /etc/fusiondirectory/fusiondirectory.conf
 sed -i -e "s/SITE/${SITE}/g" /etc/fusiondirectory/fusiondirectory.conf
+sed -i -e "s/localhost/${SENDMAIL_HOST}/g" /etc/msmtprc
 
 exec "$@"
